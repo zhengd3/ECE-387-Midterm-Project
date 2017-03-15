ccd409e6dd0d835136feb47fe4ab4873022e9e85
zhengd3 <zhengd3@miamioh.edu>
2017-03-14 21:49:19 -0400

    changed
    updated 3/14

--- ECE 387 Midterm Project Code.ino
+++ ECE 387 Midterm Project Code.ino
@@ -1,4 +1,6 @@
 //Constants:
+const int ledPinR = 3;   //pin 3 has PWM function
+const int ledPinG = 6;	 // pin 6 has PWM function
 const int flexPin = A0;  //pin A0 to read analog input
 
 //Variables:
@@ -11,11 +13,22 @@ const float VCC = 5.0;			// Voltage source
 
 void setup()
 {
+  
+  pinMode(ledPinR, OUTPUT);  //Set pin 3 as 'output'
+  pinMode(ledPinG, OUTPUT);
   Serial.begin(9600);       //Begin serial communication
+
 }
 
 void loop()
 {
+  value = analogRead(flexPin);         //Read and save analog value from flex sensor
+  Serial.println("Value is: " + String(value));  //Print value
+  int valueR = map(value, 50, 700, 0, 255);//Map value 0-1023 to 0-255 (PWM)
+  int valueG = map(value, 700, 250, 0, 255);
+  analogWrite(ledPinR, valueR);          //Send PWM value to led
+  analogWrite(ledPinG, valueG);
+  
    // Read the analog value (ADC), and calculate voltage and resistance from it
   float Voltage = value * VCC / 1023.0;
   float Resistance = R2 * (VCC / Voltage - 1.0);
